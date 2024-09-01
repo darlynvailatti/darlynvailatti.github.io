5b3cf058eacbb0bd2b01d0594f71d0228ea45d4e
darlynvailatti <darlynvailatti@users.noreply.github.com>
2024-09-01 07:06:54 +0000

    deploy: e7b862e4e48e32fa3f7eae6731e8d5718f52986d

--- src/components/DefaultIcon.tsx
+++
@@ -1,9 +0,0 @@
-import { Tooltip } from "@mui/material";
-
-export default function DefaultIcon({ size = 30, component: IconComponent, name = ""}: any) {
-    return <Tooltip title={name}>
-      <div>
-        <IconComponent size={size} />
-      </div>
-    </Tooltip>
-  }
