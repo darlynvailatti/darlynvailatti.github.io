59be409cf9347c999e13dd9dc471ecd1c11e7441
Darlyn Anderson Vailatti <darlynvailatti@gmail.com>
2024-09-20 18:35:35 +0930

    Experience + Education mobile view

--- src/containers/Education.tsx
+++ src/containers/Education.tsx
@@ -1,4 +1,4 @@
-import { Card, Chip, Grid2, Link, Stack, Typography, useMediaQuery } from "@mui/material";
+import { Box, Card, CardContent, CardHeader, Chip, Divider, Grid2, Link, Stack, Typography, useMediaQuery } from "@mui/material";
 import { CalendarMonth, PinDrop } from "@mui/icons-material";
 import { Timeline, TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineOppositeContent, TimelineSeparator } from "@mui/lab";
 import { useTheme } from "@emotion/react";
@@ -6,103 +6,165 @@ import {
     timelineOppositeContentClasses,
 } from '@mui/lab/TimelineOppositeContent';
 import { EDUCATION_CHAPTERS } from "../common/data";
+import { useMemo } from "react";
 
 export function Education() {
     const theme: any = useTheme();
-    const isMdOrLg = useMediaQuery(theme.breakpoints.up('md'));
+    const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
+    const TimelineView = useMemo(() => () => {
+        return <Timeline
+            position="right"
+            sx={{
+                padding: 0,
+                [`& .${timelineOppositeContentClasses.root}`]: {
+                    flex: 0,
+                },
+            }}>
+            {EDUCATION_CHAPTERS.map((chapter, index) => {
+                return (
+                    <TimelineItem key={index}>
+                        <TimelineOppositeContent sx={{
+                            textAlign: 'right',
+                            paddingLeft: 0,
+                        }}>
+                            <Grid2 container spacing={1}>
+
+                                <Chip
+                                    label={
+                                        <Typography variant={"caption"}
+                                            fontWeight={"bold"}>
+                                            {chapter.endDate}
+                                        </Typography>
+                                    }
+                                    size="small"
+                                    icon={<CalendarMonth />}
+                                />
+
+
+                                <Chip
+                                    label={
+                                        <Typography
+                                            variant={"caption"}
+                                            fontWeight={"bold"}>
+                                            {chapter.startDate}
+                                        </Typography>
+                                    }
+                                    size="small" icon={<CalendarMonth />}
+                                />
+
+                            </Grid2>
+                        </TimelineOppositeContent>
+                        <TimelineSeparator>
+                            <TimelineDot>
+                                {chapter.icon}
+                            </TimelineDot>
+
+                            <TimelineConnector />
+
+                        </TimelineSeparator>
+
+                        <TimelineContent sx={{
+                            paddingRight: 0,
+                        }}>
+                            <Card variant="outlined" sx={{
+                                padding: 2,
+                                textAlign: 'left',
+                                margin: 1,
+                                marginRight: 0,
+                            }}>
+                                <Stack spacing={1}>
+                                    <Link href={chapter.website} target="_blank" rel="noopener noreferrer">
+                                        <Typography variant="h5" fontWeight={"bold"}>{chapter.institution}</Typography>
+                                    </Link>
+                                    <Grid2 container spacing={1}>
+                                        <Grid2>
+                                            <PinDrop sx={{ height: 20, width: 20 }} />
+                                        </Grid2>
+                                        <Grid2>
+                                            <Typography variant="body2" fontWeight={"bold"}>{chapter.location}</Typography>
+                                        </Grid2>
+                                    </Grid2>
+                                    <Typography variant="body1" fontWeight={"bold"}>{chapter.title}</Typography>
+
+                                    <Grid2 container spacing={1}>
+                                        {chapter.tags.map((tag, index) => (
+                                            <Grid2 key={index}>
+                                                <Chip label={tag} />
+                                            </Grid2>
+                                        ))}
+                                    </Grid2>
+                                </Stack>
+                            </Card>
+                        </TimelineContent>
+                    </TimelineItem>
+                )
+            })}
+        </Timeline>
+    }, []);
+
+    const TimelineViewMobile = useMemo(() => () => {
+        return (<Stack>
+            {EDUCATION_CHAPTERS.map((chapter, index) => {
+                return (
+                    <Card key={index} variant="outlined" sx={{ margin: 1 }}>
+                        <CardHeader
+                            avatar={chapter.icon}
+                            title={<Link href={chapter.website} target="_blank" rel="noopener noreferrer">
+                                <Typography variant="body1" fontWeight={"bold"}>{chapter.institution}</Typography>
+                            </Link>} subheader={chapter.title} />
+
+                        <Divider sx={{ marginLeft: -2, marginRight: -2 }} />
+
+                        <CardContent>
+                            <Stack spacing={2}>
+
+                                <Grid2 container spacing={1}>
+
+                                    <Chip label={
+                                        <Typography
+                                            variant={"caption"}
+                                            fontWeight={"bold"}>
+                                            {chapter.startDate}
+                                        </Typography>}
+                                        size="small" icon={<CalendarMonth />}
+                                    />
+
+                                    <Chip
+                                        label={
+                                            <Typography variant={"caption"}
+                                                fontWeight={"bold"}>
+                                                {chapter.endDate}
+                                            </Typography>}
+                                        size="small" icon={<CalendarMonth />}
+                                    />
+                                </Grid2>
+                                <Box display={"flex"} alignItems={"center"}>
+                                    <PinDrop sx={{ height: 20, width: 20 }} />
+                                    <Typography variant="body2">{chapter.location}</Typography>
+                                </Box>
+
+                                <Grid2 container spacing={1}>
+                                    {chapter.tags.map((tag, index) => (
+                                        <Grid2 key={index}>
+                                            <Chip label={tag} />
+                                        </Grid2>
+                                    ))}
+                                </Grid2>
+                            </Stack>
+                        </CardContent>
+                    </Card>
+                )
+            })}
+        </Stack>)
+    }, []);
 
     return (
         <Stack spacing={2} textAlign={"left"}>
 
             <Typography variant="h5" fontWeight="bold">Education</Typography>
-            <Timeline
-                position="right"
-                sx={{
-                    padding: 0,
-                    [`& .${timelineOppositeContentClasses.root}`]: {
-                        flex: 0,
-                    },
-                }}>
-                {EDUCATION_CHAPTERS.map((chapter, index) => {
-                    return (
-                        <TimelineItem key={index}>
-                            <TimelineOppositeContent sx={{
-                                textAlign: 'right',
-                                paddingLeft: 0,
-                            }}>
-                                <Grid2 container spacing={1}>
 
-                                    <Chip
-                                        label={
-                                            <Typography variant={isMdOrLg ? "body2" : "caption"}
-                                                fontWeight={"bold"}>
-                                                {chapter.endDate}
-                                            </Typography>
-                                        }
-                                        size="small"
-                                        icon={<CalendarMonth />}
-                                    />
-
-
-                                    <Chip
-                                        label={
-                                            <Typography
-                                                variant={isMdOrLg ? "body2" : "caption"}
-                                                fontWeight={"bold"}>
-                                                {chapter.startDate}
-                                            </Typography>
-                                        }
-                                        size="small" icon={<CalendarMonth />}
-                                    />
-
-                                </Grid2>
-                            </TimelineOppositeContent>
-                            <TimelineSeparator>
-                                {isMdOrLg ? <TimelineDot>
-                                    {chapter.icon}
-                                </TimelineDot> : <TimelineDot />}
-
-                                <TimelineConnector />
-
-                            </TimelineSeparator>
-
-                            <TimelineContent sx={{
-                                paddingRight: 0,
-                            }}>
-                                <Card variant="outlined" sx={{
-                                    padding: 2,
-                                    textAlign: 'left',
-                                    margin: 1,
-                                    marginRight: 0,
-                                }}>
-                                    <Stack spacing={1}>
-                                        <Link href={chapter.website} target="_blank" rel="noopener noreferrer">
-                                            <Typography variant="h5" fontWeight={"bold"}>{chapter.institution}</Typography>
-                                        </Link>
-                                        <Grid2 container spacing={1}>
-                                            <Grid2>
-                                                <PinDrop sx={{ height: 20, width: 20 }} />
-                                            </Grid2>
-                                            <Grid2>
-                                                <Typography variant="body2" fontWeight={"bold"}>{chapter.location}</Typography>
-                                            </Grid2>
-                                        </Grid2>
-                                        <Typography variant="body1" fontWeight={"bold"}>{chapter.title}</Typography>
-
-                                        <Grid2 container spacing={1}>
-                                            {chapter.tags.map((tag, index) => (
-                                                <Grid2 key={index}>
-                                                    <Chip label={tag} />
-                                                </Grid2>
-                                            ))}
-                                        </Grid2>
-                                    </Stack>
-                                </Card>
-                            </TimelineContent>
-                        </TimelineItem>
-                    )
-                })}
-            </Timeline>
+            {isMobile ? <TimelineViewMobile /> : <TimelineView />}
 
         </Stack>
     );
--- src/containers/Experience.tsx
+++ src/containers/Experience.tsx
@@ -1,4 +1,4 @@
-import { Card, Chip, Grid2, Link, Stack, Typography, useMediaQuery } from "@mui/material";
+import { Box, Card, CardContent, CardHeader, Chip, Divider, Grid2, Link, Stack, Typography, useMediaQuery } from "@mui/material";
 import Timeline from '@mui/lab/Timeline';
 import TimelineItem from '@mui/lab/TimelineItem';
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
@@ -11,105 +11,156 @@ import TimelineOppositeContent, {
     timelineOppositeContentClasses,
 } from '@mui/lab/TimelineOppositeContent';
 import { useTheme } from "@emotion/react";
+import { useMemo } from "react";
 
 
 export function Experience() {
 
     const theme: any = useTheme();
-    const isMdOrLg = useMediaQuery(theme.breakpoints.up('md'));
+    const isMobile = useMediaQuery(theme.breakpoints.down('md'));
 
-    return (
-        <Stack spacing={1} textAlign={"left"}>
-            <Typography variant="h5" fontWeight="bold">Experience</Typography>
-            <Timeline
-                position="right"
-                sx={{
-                    padding: 0,
-                    [`& .${timelineOppositeContentClasses.root}`]: {
-                        flex: 0,
-                    },
-                }}>
-                {EXPERIENCES.map((experience, index) => {
-                    const description = isMdOrLg ? experience.description : experience.description.substring(0, 100) + "...";
-                    return (
-                        <TimelineItem key={index}>
-                            <TimelineOppositeContent sx={{
-                                textAlign: 'right',
-                                paddingLeft: 0,
+    const TimelineView = useMemo(() => () => {
+        return <Timeline
+            position="right"
+            sx={{
+                padding: 0,
+                [`& .${timelineOppositeContentClasses.root}`]: {
+                    flex: 0,
+                },
+            }}>
+            {EXPERIENCES.map((experience, index) => {
+                const description = experience.description;
+                return (
+                    <TimelineItem key={index}>
+
+                        <TimelineOppositeContent sx={{
+                            textAlign: 'right',
+                            paddingLeft: 0,
+                        }}>
+                            <Grid2 container spacing={1}>
+
+                                <Chip
+                                    label={
+                                        <Typography variant={"caption"}
+                                            fontWeight={"bold"}>
+                                            {experience.endDate}
+                                        </Typography>}
+                                    size="small" icon={<CalendarMonth />}
+                                />
+
+
+                                <Chip label={
+                                    <Typography
+                                        variant={"caption"}
+                                        fontWeight={"bold"}>
+                                        {experience.startDate}
+                                    </Typography>}
+                                    size="small" icon={<CalendarMonth />}
+                                />
+
+                            </Grid2>
+                        </TimelineOppositeContent>
+                        <TimelineSeparator>
+                            <TimelineDot>
+                                {experience.icon}
+                            </TimelineDot>
+
+                            <TimelineConnector />
+
+                        </TimelineSeparator>
+
+                        <TimelineContent sx={{
+                            paddingRight: 0,
+                        }}>
+                            <Card variant="outlined" sx={{
+                                padding: 2,
+                                textAlign: 'left',
+                                margin: 1,
+                                marginRight: 0,
                             }}>
+                                <Stack spacing={1}>
+                                    <Link href={experience.website} target="_blank" rel="noopener noreferrer">
+                                        <Typography variant="h5" fontWeight={"bold"}>{experience.company}</Typography>
+                                    </Link>
+                                    <Grid2 container spacing={1}>
+                                        <Grid2>
+                                            <PinDrop sx={{ height: 20, width: 20 }} />
+                                        </Grid2>
+                                        <Grid2>
+                                            <Typography variant="body2" fontWeight={"bold"}>{experience.location}</Typography>
+                                        </Grid2>
+                                    </Grid2>
+                                    <Typography variant="body1" fontWeight={"bold"}>{experience.title}</Typography>
+                                    <Typography variant="body2">
+                                        {description}
+                                    </Typography>
+
+                                    <Grid2 container spacing={1}>
+                                        {experience.tags.map((tag, index) => (
+                                            <Grid2 key={index}>
+                                                <Chip label={tag} />
+                                            </Grid2>
+                                        ))}
+                                    </Grid2>
+                                </Stack>
+                            </Card>
+                        </TimelineContent>
+                    </TimelineItem>
+                )
+            })}
+        </Timeline>
+    }, [])
+
+    const SmallTimelineView = useMemo(() => () => {
+        return <Stack>
+            {EXPERIENCES.map((experience, index) => {
+                return (
+                    <Card variant="outlined" sx={{ margin: 1 }}>
+                        <CardHeader avatar={experience.icon} title={experience.company} subheader={experience.title} />
+                        
+                        <Divider sx={{marginLeft: -2, marginRight: -2}}/>
+                        <CardContent>
+                            <Stack key={index} spacing={2}>
                                 <Grid2 container spacing={1}>
-
-                                    <Chip
-                                        label={
-                                            <Typography variant={isMdOrLg ? "body2" : "caption"}
-                                                fontWeight={"bold"}>
-                                                {experience.endDate}
-                                            </Typography>}
-                                        size="small" icon={<CalendarMonth />}
-                                    />
-
 
                                     <Chip label={
                                         <Typography
-                                            variant={isMdOrLg ? "body2" : "caption"}
+                                            variant={"caption"}
                                             fontWeight={"bold"}>
                                             {experience.startDate}
                                         </Typography>}
                                         size="small" icon={<CalendarMonth />}
                                     />
 
+                                    <Chip
+                                        label={
+                                            <Typography variant={"caption"}
+                                                fontWeight={"bold"}>
+                                                {experience.endDate}
+                                            </Typography>}
+                                        size="small" icon={<CalendarMonth />}
+                                    />
                                 </Grid2>
-                            </TimelineOppositeContent>
-                            <TimelineSeparator>
-                                {isMdOrLg ? <TimelineDot>
-                                    {experience.icon}
-                                </TimelineDot> : <TimelineDot />}
+                                <Box display={"flex"} alignItems={"center"}>
+                                    <PinDrop sx={{ height: 20, width: 20 }} />
+                                    <Typography variant="body2">{experience.location}</Typography>
+                                </Box>
+                                <Divider />
+                                <Typography variant="body2">{experience.description}</Typography>
+                            </Stack>
 
-                                <TimelineConnector />
+                        </CardContent>
+                    </Card>
+                )
+            })}
+        </Stack>
+    }, [])
 
-                            </TimelineSeparator>
+    return (
+        <Stack spacing={1} textAlign={"left"}>
+            <Typography variant="h5" fontWeight="bold">Experience</Typography>
 
-                            <TimelineContent sx={{
-                                paddingRight: 0,
-                            }}>
-                                <Card variant="outlined" sx={{
-                                    padding: 2,
-                                    textAlign: 'left',
-                                    margin: 1,
-                                    marginRight: 0,
-                                }}>
-                                    <Stack spacing={1}>
-                                        <Link href={experience.website} target="_blank" rel="noopener noreferrer">
-                                            <Typography variant="h5" fontWeight={"bold"}>{experience.company}</Typography>
-                                        </Link>
-                                        <Grid2 container spacing={1}>
-                                            <Grid2>
-                                                <PinDrop sx={{ height: 20, width: 20 }} />
-                                            </Grid2>
-                                            <Grid2>
-                                                <Typography variant="body2" fontWeight={"bold"}>{experience.location}</Typography>
-                                            </Grid2>
-                                        </Grid2>
-                                        <Typography variant="body1" fontWeight={"bold"}>{experience.title}</Typography>
-                                        <Typography variant="body2">
-                                            {description}
-                                        </Typography>
-
-                                        <Grid2 container spacing={1}>
-                                            {experience.tags.map((tag, index) => (
-                                                <Grid2 key={index}>
-                                                    <Chip label={tag} />
-                                                </Grid2>
-                                            ))}
-                                        </Grid2>
-                                    </Stack>
-                                </Card>
-                            </TimelineContent>
-                        </TimelineItem>
-                    )
-                })}
-            </Timeline>
-
+            {isMobile ? <SmallTimelineView /> : <TimelineView />}
 
         </Stack>
     );
