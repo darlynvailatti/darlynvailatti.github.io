0982f95439df7fd5e35ad6e965b26a11a1d79afb
Darlyn Anderson Vailatti <darlynvailatti@gmail.com>
2024-12-27 08:02:15 +1030

    Adds transition effect

--- src/containers/Skills.tsx
+++ src/containers/Skills.tsx
@@ -1,5 +1,6 @@
 import { Chip, Divider, Grid2, Paper, Stack, Typography } from "@mui/material";
 import { SKILLS } from "../common/data";
+import { motion } from "framer-motion";
 
 export function Skills() {
   return (
@@ -8,35 +9,46 @@ export function Skills() {
 
       <Grid2 container spacing={2}>
 
+
         {SKILLS.map((skill, index) => (
-          <Grid2 key={index} size={{xs: 12, md: 6, sm: 6}}>
-            <Paper variant="outlined" sx={{
-              padding: 2,
-              display: 'flex',
-              height: '100%',
-            }}>
-              <Stack spacing={2}>
-                <Typography variant="h6" fontWeight={"bold"}>{skill.title}</Typography>
-                <Typography variant="body2" dangerouslySetInnerHTML={{ __html: skill.description }} />
 
-                <Grid2 container spacing={2}>
-                  {skill.tools.map((tool, index) => (
-                    <Grid2 key={index}>
-                      {tool}
-                    </Grid2>
-                  ))}
-                </Grid2>
-                <Divider />
-                <Grid2 container spacing={1}>
-                  {skill.tags.map((tag, index) => (
-                    <Grid2 key={index}>
-                      <Chip label={tag} />
-                    </Grid2>
-                  ))}
+          <Grid2 key={index} size={{ xs: 12, md: 6, sm: 6 }}>
+            <motion.div
+              initial={{ scale: 0.8 }}
+              animate={{ scale: 1 }}
+              transition={{
+                duration: 0.5,
+                scale: { type: "spring", visualDuration: 0.5, bounce: 0 },
+              }}
+            >
+              <Paper variant="outlined" sx={{
+                padding: 2,
+                display: 'flex',
+                height: '100%',
+              }}>
+                <Stack spacing={2}>
+                  <Typography variant="h6" fontWeight={"bold"}>{skill.title}</Typography>
+                  <Typography variant="body2" dangerouslySetInnerHTML={{ __html: skill.description }} />
 
-                </Grid2>
-              </Stack>
-            </Paper>
+                  <Grid2 container spacing={2}>
+                    {skill.tools.map((tool, index) => (
+                      <Grid2 key={index}>
+                        {tool}
+                      </Grid2>
+                    ))}
+                  </Grid2>
+                  <Divider />
+                  <Grid2 container spacing={1}>
+                    {skill.tags.map((tag, index) => (
+                      <Grid2 key={index}>
+                        <Chip label={tag} />
+                      </Grid2>
+                    ))}
+
+                  </Grid2>
+                </Stack>
+              </Paper>
+            </motion.div>
 
           </Grid2>
         ))}
